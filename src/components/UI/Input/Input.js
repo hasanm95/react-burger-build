e04596891f22c7144b90e2286e167e05f29160f5
Hasan Mobarak <hsncool8@gmail.com>
2019-10-30 23:21:44 +0600

    Added form submission

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Input.css';
+
+const Input = props => {
+    let inputElement = null;
+    switch(props.elementType){
+        case 'text':
+            inputElement = <input 
+                            className="InputElement" 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed}/>;
+            break;
+        case 'textarea':
+            inputElement = <textarea 
+                            className="InputElement" 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed}></textarea>;
+            break;
+        case 'select':
+            inputElement = <select
+                            className="InputElement"
+                            value={props.value}
+                            onChange={props.changed}>
+                            {props.elementConfig.options.map(el => (
+                                <option value={el.value} key={el.value}>{el.displayValue}</option>
+                            ))}
+            </select>;
+            break;
+        default:
+            inputElement = <input 
+                            className="InputElement" 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed}/>;
+    }
+    return (
+        <div className="Input">
+            <label className="Label">{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+export default Input
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,25 +3,73 @@ import Button from '../../../components/UI/Button/Button'
 import './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input';
 
 const ContactData = props => {
+    const [orderForm, setOrderForm] = useState({
+        name: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Your Name'
+            },
+            value: ''
+        },
+        street: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Your Street'
+            },
+            value: ''
+        },
+        zipCode: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Your Zipcode'
+            },
+            value: ''
+        },
+        country: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Your Country'
+            },
+            value: ''
+        },
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Your Email'
+            },
+            value: ''
+        },
+        deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+                options: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ]
+            },
+            value: 'fastest'
+        }
+    })
     const [loading, setLoading] = useState(false)
     const orderHandler = event => {
         event.preventDefault();
         setLoading(true);
+        const formData = {};
+        for(let formEl in orderForm){
+            formData[formEl] = orderForm[formEl].value;
+        }
         const order = {
             ingredients: props.ingredients,
             price: props.price,
-            customer: {
-                name: 'Hasan Mobarak',
-                address: {
-                    street: 'test street',
-                    zipCode: '3343',
-                    country: 'Bangladesh'
-                },
-                email: 'test@test.com',
-                deliveryMethod: 'fastest'
-            }
+            orderData: formData
         }
         axios.post('/orders.json', order)
         .then(response => {
@@ -33,13 +81,36 @@ const ContactData = props => {
         })
     }
 
+    const onChangeHandler = (event, inputindentifier) => {
+        const updatedForm = {...orderForm};
+        const updatedFormEl = {...updatedForm[inputindentifier]}
+        updatedFormEl.value = event.target.value;
+        updatedForm[inputindentifier] = updatedFormEl;
+        setOrderForm(updatedForm);
+    }
+
+    const formElementArray = [];
+    for(let key in orderForm){
+        formElementArray.push({
+            id: key,
+            config: orderForm[key]
+        })
+    }
     let form = (
-        <form>
-            <input className="Input" type="text" name="name" placeholder="Your Name"/>
-            <input className="Input" type="text" name="email" placeholder="Your Email"/>
-            <input className="Input" type="text" name="street" placeholder="Your Street"/>
-            <input className="Input" type="text" name="postCode" placeholder="Your Postacod"/>
-            <Button btnType="Success" clicked={orderHandler}>Order</Button>
+        <form onSubmit={orderHandler}>
+            {
+                formElementArray.map(formElement => {
+                    const {id, config: {elementType, value, elementConfig}} = formElement;
+                    return <Input 
+                        key={id}
+                        elementType={elementType} 
+                        elementConfig={elementConfig}
+                        value={value}
+                        changed={(event) => onChangeHandler(event, id)}
+                    />
+                })
+            }
+            <Button btnType="Success">Order</Button>
         </form>
     )
 
